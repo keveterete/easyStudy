f59a3a9301f37f9b39ab1105f378750e8c676c72
Ilya Tarasov <growbykev@gmail.com>
2021-06-23 16:00:57 +0500

    Добавьте файлы проекта.

---
+++ easyStudy/Form2.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace easyStudy
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.answer1 = new System.Windows.Forms.Button();
+            this.answer2 = new System.Windows.Forms.Button();
+            this.answer3 = new System.Windows.Forms.Button();
+            this.answer4 = new System.Windows.Forms.Button();
+            this.exit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(62, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // answer1
+            // 
+            this.answer1.Location = new System.Drawing.Point(10, 118);
+            this.answer1.Name = "answer1";
+            this.answer1.Size = new System.Drawing.Size(100, 100);
+            this.answer1.TabIndex = 1;
+            this.answer1.Text = "button1";
+            this.answer1.UseVisualStyleBackColor = true;
+            this.answer1.Click += new System.EventHandler(this.answer1_Click);
+            // 
+            // answer2
+            // 
+            this.answer2.Location = new System.Drawing.Point(116, 118);
+            this.answer2.Name = "answer2";
+            this.answer2.Size = new System.Drawing.Size(100, 100);
+            this.answer2.TabIndex = 2;
+            this.answer2.Text = "button2";
+            this.answer2.UseVisualStyleBackColor = true;
+            this.answer2.Click += new System.EventHandler(this.answer2_Click);
+            // 
+            // answer3
+            // 
+            this.answer3.Location = new System.Drawing.Point(10, 224);
+            this.answer3.Name = "answer3";
+            this.answer3.Size = new System.Drawing.Size(100, 100);
+            this.answer3.TabIndex = 3;
+            this.answer3.Text = "button3";
+            this.answer3.UseVisualStyleBackColor = true;
+            this.answer3.Click += new System.EventHandler(this.answer3_Click);
+            // 
+            // answer4
+            // 
+            this.answer4.Location = new System.Drawing.Point(116, 224);
+            this.answer4.Name = "answer4";
+            this.answer4.Size = new System.Drawing.Size(100, 100);
+            this.answer4.TabIndex = 4;
+            this.answer4.Text = "button4";
+            this.answer4.UseVisualStyleBackColor = true;
+            this.answer4.Click += new System.EventHandler(this.answer4_Click);
+            // 
+            // exit
+            // 
+            this.exit.Location = new System.Drawing.Point(10, 340);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(206, 23);
+            this.exit.TabIndex = 5;
+            this.exit.Text = "Закрыть";
+            this.exit.UseVisualStyleBackColor = true;
+            this.exit.Click += new System.EventHandler(this.exit_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(227, 384);
+            this.Controls.Add(this.exit);
+            this.Controls.Add(this.answer4);
+            this.Controls.Add(this.answer3);
+            this.Controls.Add(this.answer2);
+            this.Controls.Add(this.answer1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button answer1;
+        private System.Windows.Forms.Button answer2;
+        private System.Windows.Forms.Button answer3;
+        private System.Windows.Forms.Button answer4;
+        private System.Windows.Forms.Button exit;
+    }
+}
---
+++ easyStudy/Form2.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace easyStudy
+{
+    public partial class Form2 : Form
+    {
+        public Dictionary<string, string> wordList = new();
+        public void refresh()
+        {
+            var lines = System.IO.File.ReadAllLines("beg.txt");
+            lines.Select(line => line.Split('='))
+                 .Where(line => line.Length == 2)
+                 .ToList()
+                 .ForEach(line => wordList.Add(line[0], line[1]));
+            Random rnd = new();
+            int wordsCount = wordList.Count;
+            int i = rnd.Next(wordsCount); //Случайное значение из словаря
+            label1.Text = wordList.ElementAt(i).Key.ToString();// Прописывание значения в слово, которое нужно перевести
+            int wheresRightWord = rnd.Next(4);
+            int a = rnd.Next(wordsCount);
+
+            while (a == i)
+            {
+                a = rnd.Next(wordsCount);
+            }
+            int b = rnd.Next(wordsCount);
+            while (b == i || b == a)
+            {
+                a = rnd.Next(wordsCount);
+            }
+            int c = rnd.Next(wordsCount);
+            while (c == i || c == a || c == b)
+            {
+                c = rnd.Next(wordsCount);
+            }
+            switch (wheresRightWord)
+            {
+                case 0:
+                    answer1.Text = wordList.ElementAt(i).Value.ToString();
+                    answer2.Text = wordList.ElementAt(a).Value.ToString();
+                    answer3.Text = wordList.ElementAt(b).Value.ToString();
+                    answer4.Text = wordList.ElementAt(c).Value.ToString();
+                    correctAnswer = 1;
+                    break;
+                case 1:
+                    answer1.Text = wordList.ElementAt(a).Value.ToString();
+                    answer2.Text = wordList.ElementAt(b).Value.ToString();
+                    answer3.Text = wordList.ElementAt(c).Value.ToString();
+                    answer4.Text = wordList.ElementAt(i).Value.ToString();
+                    correctAnswer = 4;
+                    break;
+                case 2:
+                    answer1.Text = wordList.ElementAt(a).Value.ToString();
+                    answer2.Text = wordList.ElementAt(b).Value.ToString();
+                    answer3.Text = wordList.ElementAt(i).Value.ToString();
+                    answer4.Text = wordList.ElementAt(c).Value.ToString();
+                    correctAnswer = 3;
+                    break;
+                default:
+                    answer1.Text = wordList.ElementAt(a).Value.ToString();
+                    answer2.Text = wordList.ElementAt(i).Value.ToString();
+                    answer3.Text = wordList.ElementAt(c).Value.ToString();
+                    answer4.Text = wordList.ElementAt(b).Value.ToString();
+                    correctAnswer = 2;
+                    break;
+            }
+            answer1.BackColor = Color.White;
+            answer2.BackColor = Color.White;
+            answer3.BackColor = Color.White;
+            answer4.BackColor = Color.White;
+        }
+        public int correctAnswer = 0;
+        public Form2()
+        {
+            InitializeComponent();
+            refresh();
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void answer1_Click(object sender, EventArgs e)
+        {
+            
+            switch(correctAnswer)
+            {
+                case 1:
+                    answer1.BackColor = Color.Green;
+                    break;
+                case 2:
+                    answer1.BackColor = Color.Red;
+                    answer2.BackColor = Color.Green;
+                    break;
+                case 3:
+                    answer1.BackColor = Color.Red;
+                    answer3.BackColor = Color.Green;
+                    break;
+                case 4:
+                    answer1.BackColor = Color.Red;
+                    answer4.BackColor = Color.Green;
+                    break;
+            }
+
+            wordList.Clear();
+            refresh();
+        }
+
+        public void exit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void answer2_Click(object sender, EventArgs e)
+        {
+            switch (correctAnswer)
+            {
+                case 2:
+                    answer2.BackColor = Color.Green;
+                    break;
+                case 1:
+                    answer2.BackColor = Color.Red;
+                    answer1.BackColor = Color.Green;
+                    break;
+                case 3:
+                    answer2.BackColor = Color.Red;
+                    answer3.BackColor = Color.Green;
+                    break;
+                case 4:
+                    answer2.BackColor = Color.Red;
+                    answer4.BackColor = Color.Green;
+                    break;
+            }
+
+            wordList.Clear();
+            refresh();
+        }
+
+        private void answer3_Click(object sender, EventArgs e)
+        {
+            switch (correctAnswer)
+            {
+                case 3:
+                    answer3.BackColor = Color.Green;
+                    break;
+                case 2:
+                    answer3.BackColor = Color.Red;
+                    answer2.BackColor = Color.Green;
+                    break;
+                case 1:
+                    answer3.BackColor = Color.Red;
+                    answer1.BackColor = Color.Green;
+                    break;
+                case 4:
+                    answer3.BackColor = Color.Red;
+                    answer4.BackColor = Color.Green;
+                    break;
+            }
+
+            wordList.Clear();
+            refresh();
+        }
+
+        private void answer4_Click(object sender, EventArgs e)
+        {
+            switch (correctAnswer)
+            {
+                case 4:
+                    answer4.BackColor = Color.Green;
+                    break;
+                case 2:
+                    answer4.BackColor = Color.Red;
+                    answer2.BackColor = Color.Green;
+                    break;
+                case 3:
+                    answer4.BackColor = Color.Red;
+                    answer3.BackColor = Color.Green;
+                    break;
+                case 1:
+                    answer4.BackColor = Color.Red;
+                    answer1.BackColor = Color.Green;
+                    break;
+            }
+
+            wordList.Clear();
+            refresh();
+        }
+    }
+}
